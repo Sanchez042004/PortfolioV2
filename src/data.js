cf0ced48ef855d96059791932f0f500238ade69a
Sanchez042004 <afsarias2004@gmail.com>
2026-01-05 23:01:45 -0500

    chore: include remaining assets and cleanup

--- src/data.js
+++ src/data.js
@@ -9,7 +9,7 @@ export const data = {
             github: CONFIG.GITHUB_URL,
             linkedin: CONFIG.LINKEDIN_URL,
         },
-        avatar: '/photo.webp',
+        avatar: '/photo1.webp',
         cv: CONFIG.RESUME_PATH
     },
     nav: [
